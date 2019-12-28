6afa91e9b650c4d88404fa4c2730d5f0dfcfa27d
Juozas Getautis <juozas.getautis@transfergo.com>
2019-12-29 00:16:22 +0200

    Add shuffle flag, trim user input

--- main.go
+++ main.go
@@ -7,7 +7,9 @@ import(
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -45,26 +47,45 @@ func readFile(filename string) []Qst{
 	return questions
 }
 
+func shuffle(list []Qst, check bool) []Qst {
+	if check == true {
+		// Seed a random number generator and generate a permutation list up to the length of our original list
+		r := rand.New(rand.NewSource(time.Now().Unix()))
+		perm := r.Perm(len(list))
+
+		shuffledList := make([]Qst, len(list))
+		for i, randIndex := range perm {
+			shuffledList[i] = list[randIndex]
+		}
+		return shuffledList
+	}
+	return list
+}
+
 func main() {
 	userInput := ""
 	var filename = flag.String("file", "problems.csv", "The problems csv file")
 	var timeSeconds = flag.Int("time", 30, "Timer value in seconds")
+	var shuffleCheck = flag.Bool("shuffle", false, "Shuffle questions true/false")
 	flag.Parse()
-
 	fmt.Println("Welcome the Quiz!")
 	questions := readFile(*filename)
-	fmt.Println(*filename)
+	questions = shuffle(questions, *shuffleCheck)
 	fmt.Println("Press enter to start [ENTER]")
 	fmt.Scanln(&userInput)
 	timer := time.NewTimer(time.Duration(*timeSeconds) * time.Second)
 	var correct int
 	for i, question := range questions {
 		fmt.Printf("Problem #%v: %v\n", i+1, question.question)
+
 		answerCh := make(chan string)
 		go func() {
 			var userInput string
-			_, err := fmt.Scanln(&userInput)
-			check(err)
+			_, err := fmt.Scanf("%s\n", &userInput)
+			if err != nil {
+				userInput = ""
+			}
+			userInput = strings.Trim(userInput, " ")
 			answerCh <- userInput
 		}()
 
@@ -73,7 +94,7 @@ func main() {
 			fmt.Printf("Time expired! You got %v correct out of %v.\n", correct, len(questions))
 			return
 		case userInput := <-answerCh:
-			if userInput == question.ans {
+			if strings.EqualFold(userInput, question.ans) {
 				correct++
 			}
 		}
